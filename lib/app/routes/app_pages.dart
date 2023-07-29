d86ac8ccfff1634c58f330e4bcb7c4554fbada15
perymerdeka <tkjasikk@gmail.com>
2023-07-29 18:40:38 +0700

    Fix: Route

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -18,7 +18,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.LOGIN_SUCCESS;
+  static const INITIAL = Routes.SPLASH;
 
   static final routes = [
     GetPage(
